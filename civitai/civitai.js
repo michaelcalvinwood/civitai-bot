0d106862bfcf438460e57219020075e8f012f851
Michael Wood <michaelwood33311@icloud.com>
2024-07-13 19:52:46 -0400

    isolated stack

--- civitai/civitai.js
+++ civitai/civitai.js
@@ -1,1 +1,13 @@
-alert ("I am here");
+
+
+console.log('the path', window.location.pathname);
+
+
+const deleteThem = () => {
+  const mrs = document.querySelectorAll('.mantine-Tabs-root.mantine-1arnq9x');
+
+  console.log('num mrs', mrs.length);
+
+}
+
+setInterval(deleteThem, 10000);
